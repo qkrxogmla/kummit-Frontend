f2c02b00945da2746aee4ef27030cd6b8afd95b9
qkrxogmla <suk05553@naver.com>
2025-06-28 22:15:26 +0900

    post로 수정

--- src/apis/createpage.ts
+++ src/apis/createpage.ts
@@ -5,9 +5,10 @@ type SearchMusic = {
 };
 
 export const getTopEmotionBands = async (searchMusic: SearchMusic) => {
-  const res = await axios.post(
-    "http://144.24.81.195:8080/api/music/search",
-    searchMusic
+  const res = await axios.get(
+    `http://144.24.81.195:8080/api/music/search?query=${encodeURIComponent(
+      searchMusic.query
+    )}`
   );
   return res.data;
 };
